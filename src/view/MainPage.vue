d4f0507a37ed2cd4691176a4c4c0afaf9c025013
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-05 00:13:48 +0900

    click, onchage and watch prac

--- src/view/MainPage.vue
+++ src/view/MainPage.vue
@@ -4,6 +4,20 @@
     <input type="text" v-model="input1" />
     <button type="button" @click="getData">Get</button>
     <button type="button" @click="setData">Set</button>
+
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <!-- 요소 안에서 접근할 때 : 사용, 값을 사용할 때 {{}} 사용 -->
+      <option :key="i" :value="d.v" v-for="(d, i) in options">
+        {{ d.t }}
+      </option>
+    </select>
+
+    <table class="table table-border">
+      <tr :key="i" v-for="(d, i) in options">
+        <td>{{ d.v }}</td>
+        <td>{{ d.t }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -16,7 +30,29 @@ export default {
       title: "개발자의 품격",
       title2: "Seoul",
       input1: "abcd",
+      options: [
+        {
+          v: "S",
+          t: "Seoul",
+        },
+        {
+          v: "J",
+          t: "Jeju",
+        },
+        {
+          v: "B",
+          t: "Busan",
+        },
+      ],
+      region: "J",
     };
+  },
+
+  watch: {
+    // input1 데이터가 변경되면 호출된다.
+    input1() {
+      console.log("watch : ", this.input1);
+    },
   },
 
   methods: {
@@ -25,6 +61,10 @@ export default {
     },
     setData() {
       this.input1 = "12345";
+    },
+    changeRegion() {
+      console.log("test");
+      alert(this.region);
     },
   },
   beforeCreate() {
