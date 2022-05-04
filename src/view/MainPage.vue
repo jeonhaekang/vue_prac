62245304947598e674133d15c4a3029118a6a581
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-04 23:47:31 +0900

    data control prac

--- src/view/MainPage.vue
+++ src/view/MainPage.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <h1>welcome to Home!</h1>
+    <h1>welcome to {{ title2 }}!</h1>
+    <input type="text" v-model="input1" />
+    <button type="button" @click="getData">Get</button>
+    <button type="button" @click="setData">Set</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "MainPage",
+
+  data() {
+    return {
+      title: "개발자의 품격",
+      title2: "Seoul",
+      input1: "abcd",
+    };
+  },
+
+  methods: {
+    getData() {
+      console.log(this.input1);
+    },
+    setData() {
+      this.input1 = "12345";
+    },
+  },
 };
 </script>
